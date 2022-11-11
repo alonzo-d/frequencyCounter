3767922880d53cc281e95f4f4cc0656ee6f03ca9
David A. Salazar <davidalonzso@gmail.com>
2022-11-11 15:49:06 -0800

    canConstructWord function

--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,31 @@
+'use strict';
+
+/** can we build first string w/ chars from second string; return boolean
+ * 
+ * number of matching chars frequency needs to match
+ * letters provided in second string need to be >= the number in first string
+ * all chars all lowercase w/ no spaces or punc.
+ * 
+ * input: two strings
+ * output: boolean
+ * 
+ * 
+ */
 // add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+function canConstructWord(word, letters) {
+    // counter obj that holds all values from word string
+    // counter obj2 that holds all values from letters string
+
+    // compare both objects for same number of values
+    
 }
+
+// canConstructWord('aa', 'abc');
+//   // false -- can't build "aa" with only 1 "a"
+
+// canConstructWord('abc', 'dcba');
+//   // true -- can build "abc" with letters "abcd"
+
+// canConstructWord('aabb', 'bcabad');
+  // true -- can build "aabbcc" with those letters
+
